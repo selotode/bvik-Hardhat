e27031ad99026a7bef21e2c4bb37d5067470124e
Ivan Markovski <markovskiivan99@gmail.com>
2022-05-18 14:23:56 +0200

    finished lab

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -10,18 +11,28 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
+
+
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
+ 
 module.exports = {
   solidity: "0.8.4",
   networks: {
+	hardhat: {
+    chainId: 31337
+  },
    rinkeby: {
-     url: "https://rinkeby.infura.io/v3/ba900937b83f4883b926713999277b1f", //Infura url with projectId
-     accounts: ["123456789101112131415"] // add the account that will deploy the contract (private key)
-    },
+     url: "https://rinkeby.infura.io/v3/caf7a70d1ca2492e8bbceed1cc45cde3",
+     accounts: ["5f22b5e458acaffaf16d360b71d4aefe4399d11908a50a9ad0d581f310b99265"]
+	 },
+  },
+   etherscan: {
+    apiKey: "KQVTU18W9QYEINXTT69GISHVBD1TDCWYNG"
   }
 };
